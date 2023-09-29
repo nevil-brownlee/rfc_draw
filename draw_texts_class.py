a4f4eac78749b1ec43420871a5d74af0923285af
Nevil Brownlee <nevil.brownlee@gmail.com>
2023-09-29 16:28:34 +1300

    Add files via upload

---
+++ draw_texts_class.py
@@ -0,0 +1,104 @@
+# 1648, Tue  6 Dec 2022 (NZDT)
+# 1539, Sat 21 Jan 2023 (NZDT)
+#
+# draw_texts_class: functions to draw/move/edit tkinter text objects
+#
+# Copyright 2023, Nevil Brownlee, Taupo NZ
+
+import tkinter as tk
+#import tkinter.font
+
+import rfc_draw_globals_class as rdgc
+
+class draw_texts:  # text objects for rfc-draw
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)  # New instance of text_info
+
+    def __init__(self, parent, root, rd_globals):
+        super().__init__()
+        self.drawing = parent;  self.root = root
+        self.rdg = rd_globals
+
+        self.tx_far = 6
+        #self.f_font = tkinter.font.Font(  # Initialise class variables
+        #    family="TkFixedFont", size=10, weight="normal")
+        self.f_font = "TkFixedFont"  # This works, above version doesn't <<<
+        #self.f_font = "Courier 10 bold"  # This does too
+        # https://stackoverflow.com/questions/48731746/
+        # how-to-set-a-tkinter-widget-to-a-monospaced-platform-independent-font
+
+    def set_event_handlers(self):
+        # Click b1 to make a create_text() object
+        self.drawing.bind_class('Canvas','<ButtonPress-1>', self.tx_b1_click)
+        self.drawing.bind_class('Canvas','<Button1-Motion>',self.tx_b1_motion)
+        self.drawing.bind_class('Canvas','<ButtonRelease-1>',self.tx_b1_release)
+
+    def tx_closest(self, mx,my):
+        item = self.drawing.find_closest(mx,my) 
+        if len(item) == 0:  # Empty tuple
+            print("rdg_closest(0): empty tuple")
+            return None, None
+        item_id = item[0]
+        item_type = self.drawing.type(item_id)
+        print("@@@ tx_closest, item_id %d, type %s" % (item_id, item_type))
+        if item_type != "text":
+            return None, None
+        print("@=@ tx_closest(1): item %s (%s), mx,my %d,%d" % (
+            item, item_type, mx,my))
+        if item_id in self.rdg.objects:
+            obj = self.rdg.objects[item_id]  # item_id is a tkinter object id
+            # object (e.g. nro) has: key, obj, obj_ type, in_n_rect
+            print("-> closest %s is in objects, obj >%s<" % (item, obj))
+            cx,cy = self.drawing.coords(item_id)  # Centre of text object
+            c_text = self.drawing.itemcget(item_id, "text")
+            h,w = self.rdg.str_h_w(c_text)  # 0.5 of dimensions!
+            px_h = h*self.rdg.f_height;  px_w = w*self.rdg.f_width
+            #print("=== mx,my %d,%d  px_w %d, px_h %d" % (mx,my, px_w,px_h))
+            ##print(">H< %s" % (abs(mx-cx) > (px_w*2 + self.tx_far)/2))
+            ##print(">V< %s" % (abs(my-cy) > (px_h*2 + self.tx_far)/2))
+            if ( (abs(mx-cx) > (px_w*2 + self.tx_far)/2) or
+                 (abs(my-cy) > (px_h*2 + self.tx_far)/2) ):
+                print("text is too far away")
+                return None, None
+            print("--- close !!!")
+            return item, obj
+        else:  # Not in objects ??
+            print("@@@ text %d is not in rdg.objects <<<<" % item_id)
+            return None, None
+
+    def tx_b1_click(self, event):  # B1 (left button) to create_text()
+        mx, my = (event.x, event.y)  # Mouse position
+        print(". . . on_b1_click_t: %d,%d, %s, %s" % (
+            mx, my, event.widget, event.type))        
+
+        item, obj = self.tx_closest(mx,my)  # Closest tk object
+        if item == None:
+            #print("no tk objects in drawing yet!")
+            tx_str = "--" + str(len(self.rdg.objects)+1) + "--"
+            text_obj = self.rdg.new_text(mx, my, tx_str, 0)
+        else:
+            print("item = %s, obj = |%s|" % (item, obj))
+            item_ix = item[0]
+            if obj.o_type == "text":
+                print("You clicked near text %d" % item_ix)
+                self.rdg.current_object = obj
+        self.rdg.last_mx = mx;  self.rdg.last_my = my  # Last mouse position
+
+    def tx_b1_motion(self, event):  # Move the current_obj_id
+        mx, my = (event.x, event.y)
+        #print("b1_motion: %d,%d" % (mx,my))
+        self.drawing.move(self.rdg.current_object.object,
+            mx-self.rdg.last_mx, my-self.rdg.last_my)
+        self.rdg.last_mx = mx;  self.rdg.last_my = my  # Last mouse position
+
+    def tx_b1_release(self, event):  # Left button released
+        mx, my = (event.x, event.y)  # Mouse position
+        self.rdg.last_mx = mx;  self.rdg.last_my = my
+
+if __name__ == "__main__":
+    root = tk.Tk()  # Main window
+    drawing = tk.Canvas(root, width=600, height=600, bg="white")  # Drawing area
+    drawing.pack(padx=10, pady=10)
+    dto = draw_texts(drawing, root)
+    root.mainloop()
+    
---
+++ rdd_io.py
@@ -0,0 +1,178 @@
+# 1107, Wed  6 Sep 2023 (NZST)
+# 1455, Wed 31 May 2023 (NZST)
+# 1457, Tue 26 Sep 2023 (NZDT)
+#
+# rdd_rw.py: Read/write *.rdd files
+#
+# Copyright 2023, Nevil Brownlee, Taupo NZ
+
+import re
+
+class rdd_rw:
+    def __init__(self, fn):
+        self.rdd_fn = fn
+        #print("rdd_fn %s" % self.rdd_fn)
+        self.objects = []   # rfc-draw objects
+        self.r_obj_keys = {}  # old-key -> new-key for objects read from rdd
+
+        # Patterns for reading the description string for an object
+        self.rrd_re = re.compile(
+               "(\d+)\s+\((.+)\s+(.+)\)\s+\[(.+)\]\s+\"(.*)\"\s+(\d+)\s+(.)")
+        # field   0        1       2          3          4         5      6
+        #       objid    type    skey       coords     text      g_nbr   g_type
+
+        self.mbr_re = re.compile(
+               "\s+\((.)(.+)\ member\s(.+)\)\s+(.+)\s\[(.+)\]")
+        # field       0  1           2        3       4
+        #    blanks  id g_nbr       g_key    type    coords
+       
+
+    def s_to_stuple(self, t):
+        t1 = t.replace("'", "")
+        cs = t1.replace('"','')
+        return cs
+
+    def s_to_ilist(self, t):
+        ol = [];  st = t.split(", ")
+        for cs in st:
+            i_d = cs.split(".")
+            ol.append(int(i_d[0]))
+        return ol
+
+    class rdd_obj:
+        def __init__(self, id, type, coords, text, t_width, g_nbr):
+            self.id = id
+            self.type = type;  self.i_coords = coords
+            self.i_text = text;  self.txt_width = t_width
+            self.g_nbr = g_nbr; self.g_members = []
+            
+        def __str__(self):
+            return (
+                "id %d, type %6s, i_text %s, g_nbr %d, i_coords %s, txt_width %d" % (
+                self.id, self.type, self.i_text, self.g_nbr,
+                self.i_coords, self.txt_width))
+
+    class g_member:  # Holds group info for a group member obj
+        def __init__(self, objects, g_nbr, m_key, g_rel_coords):
+            self.objects = objects
+            self.g_nbr = g_nbr
+            self.m_key = m_key  # member's original objects[] key
+            self.g_rel_coords = g_rel_coords
+            
+        def __str__(self):
+            m_type = self.objects[self.m_key].type
+            return ("g_nbr %s, m_key %s, m_type %s, rel_coords %s" % (
+                self.g_nbr, self.m_key, m_type, self.g_rel_coords))
+
+    def find_group_key(self, g_nbr):
+        for val in self.objects:
+            if val.type == "group" and val.g_nbr == g_nbr:
+                return val.id
+
+    def rel_coords(self, edr, s_coords):  # s to r
+        n_points = int(len(s_coords)/2)
+        #print("++ rel_coords: n_points %d, edr %s, s_coords %s" % (
+        #    n_points, edr, s_coords))
+        ex = edr[0];  ey = edr[1]  # Top-left corner
+        rel_coords = []
+        for ns in range(0,n_points):
+            rel_coords.append(s_coords[ns*2]-ex)    # sx
+            rel_coords.append(s_coords[ns*2+1]-ey)  # sy
+        #print("== rel_coords >%s<" % rel_coords)
+        return rel_coords
+
+    def restore_object(self, ds):
+        #print("restore_object: ds >%s<" % ds)
+        if "member" in ds:
+            #print("=== member line >%s<" % ds)
+            self.dump_objects(">> 'member' read <<")
+
+            fields = self.mbr_re.search(ds).groups()
+            gt = fields[0]  # 'g'
+            g_nbr = int(fields[1])  # group (or template number)
+            o_key = fields[2]  # member key in objects[]
+            m_key = self.r_obj_keys[o_key]
+                # member key in new objects[] dict
+            m_type = fields[3]  # member type
+            mo = self.objects[m_key]
+            #print("+++ mo >%s<" % mo)
+            i_coords = self.s_to_ilist(fields[4])
+            #print("gt %s, g_nbr %d m_key %d, type %s, coords >%s<" %
+            #      (gt, g_nbr, m_key, m_type, i_coords))
+            g_key = self.find_group_key(g_nbr)-1  # 0-ord for objects[]
+            go = self.objects[g_key]
+            i_coords = mo.i_coords;  i_text = mo.i_text
+            r_coords = self.rel_coords(go. i_coords, i_coords)
+            m = self.g_member(self.objects, g_nbr, m_key, r_coords)
+            self.objects[g_key].g_members.append(m)
+            return m_key, g_key
+        else:  # Ordinary object
+            #print("== ds %s ==" % ds)
+            fields = self.rrd_re.search(ds).groups()
+            #print("@@@ fields = ", end="");  print(fields)
+            obj_id = int(fields[0])  # Ignore line_nbr (field 0)
+            obj_type = fields[1]
+            s_key = fields[2]  # object's key in save file
+            coords = self.s_to_ilist(fields[3])
+            text = fields[4].replace("\\n", "\n")
+            g_nbr = int(fields[5])  # Group nbr
+            g_type = fields[6]  # Group type
+            t_width = 0
+            if obj_type == "text":
+                t_lines = text.split("\n")
+                #print("@@@ t_lines >%s<" % t_lines)
+                for l_txt in t_lines:
+                    chrs = len(l_txt)
+                    if chrs > t_width:
+                        t_width = chrs
+            obj = self.rdd_obj(obj_id, obj_type, coords, text, t_width, g_nbr)
+            #print("+++ obj = >%s<" % obj)
+            self.objects.append(obj)  # rdd_io objects are in a list 
+            new_key = len(self.objects)-1
+            self.r_obj_keys[s_key] = new_key
+            #print("r_obj_keys = %s" % self.r_obj_keys)
+            return new_key, obj
+
+    def dump_objects(self, header):
+        return
+        print("dump_objects -- %s --" % header)
+        for j, val in enumerate(self.objects):
+            print("%4d val >%s<" % (j, val))
+            if val.type == "group":
+                for g_m in val.g_members:
+                    print("  member %s" % g_m)
+        print("- - dump - -")  # Trailer line
+
+    
+    def read_from_rdd(self):
+        f = open(self.rdd_fn, "r")
+        for line in f:
+            #print(">>>%s<" % line)
+            if line[0] == "#":  # Ignore comment lines
+                continue
+            ds = line.rstrip('\n')
+            #print("ds >%s<" % ds)
+            if ds.find("root_geometry") >= 0:
+                la = ds.split(" ");  dims = la[1].split("+")
+                xy = dims[0].split("x")
+                self.xr = int(xy[0]);  self.yr = int(xy[1])
+                #print("root geometry: x %d, y %d" % (self.xr, self.yr))
+            elif ds.find("drawing_size") >= 0:
+                la = ds.split(" ")
+                la_ds = la[1].split("x")
+                self.dw = int(la_ds[0])  # drawing width
+                self.dh = int(la_ds[1])  # drawing height
+                #print("drawing_size %dx%d" % (self.dw,self.dh))
+            elif ds.find("mono_font") >= 0:
+                la = ds.split(" ")
+                self.f_width = float(la[2])
+                self.f_height = int(la[4])
+                #print("mono_font width %.2f, height %.2f pixels" % (
+                #    self.f_width, self.f_height))
+            else:
+                #print("=== ds = %s" % ds)
+                o_key, obj = self.restore_object(ds)
+                #print("--- o_key %s, obj >%s<" % (o_key, obj))
+        return self.xr, self.yr, self.f_width, self.f_height, self.dw, self.dh
+                # xr,ry are rfc-draw's 'drawing' dimension (in px)
+    
