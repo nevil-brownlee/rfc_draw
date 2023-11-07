2862d3508c4020e7a4778c39df7a9cfc4f690742
Nevil Brownlee <nevil.brownlee@gmail.com>
2023-11-07 15:54:57 +1300

    Update rdd-to-*, add rdd_globals and rdd-to-xmlfig

--- rdd-to-xmlfig.py
+++ rdd-to-xmlfig.py
@@ -13,7 +13,6 @@ class xmlfig_drawing:
         print("rdd_fn %s, asc %s, svg %s" % (rdd_fn, asc_b_w, svg_b_w))
 
         rdd_to_ascii = __import__("rdd-to-ascii")
-        b_w_option = "-b%s" % asc_b_w
         if asc_b_w:
             ascii_obj = rdd_to_ascii.asc_drawing(["dummy", rdd_fn, asc_b_w])
         else:
@@ -24,7 +23,6 @@ class xmlfig_drawing:
             ascii_obj.rdd_fn, ascii_obj.border_width))
 
         rdd_to_svg = __import__("rdd-to-svg")
-        b_w_option = "-b%s" % asc_b_w
         if svg_b_w:
             svg_obj = rdd_to_svg.svg_drawing(["dummy", rdd_fn, svg_b_w])
         else:
--- rdd_globals.py
+++ rdd_globals.py
@@ -1,4 +1,4 @@
-# 1410, Thu  2 Nov 2023 (NZDT)
+# 1036, Fri  3 Nov 2023 (NZDT)
 #
 # rdd_globals: globals for rfc-draaw's rdd-to*.py programs
 #
@@ -6,7 +6,9 @@
 
 class rdd_globals:
     def __init__(self):
-        self.asc_def_b_w = 1  # chars/lines
-        self.svg_def_b_w = 3  # pixels
+        self.asc_def_b_w = 1  # border_width chars/lines
+        self.svg_def_b_w = 3  #              pixels
+        self.asc_x_sf = 1.5   # ascii x scale factor
+        self.asc_y_sf = 0.9   #       y
  
 gv = rdd_globals()  # rdd global variables
